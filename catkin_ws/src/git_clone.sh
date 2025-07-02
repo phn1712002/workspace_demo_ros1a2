e013742c853009f878ad309b32872d70805e37f4
phn1712002 <phn1712002@gmail.com>
2025-07-02 15:02:14 +0700

    Add ROS index URL comment to git_clone scripts
    Include reference to https://index.ros.org/ in both catkin_ws and colcon_ws
    git_clone.sh scripts to provide quick access to ROS package index. Maintains
    existing clone commands while adding helpful documentation.

--- catkin_ws/src/git_clone.sh
+++ catkin_ws/src/git_clone.sh
@@ -1,2 +1,3 @@
+# https://index.ros.org/
 # git clone {URL}
 git clone https://github.com/ros-drivers/rosserial.git -b noetic-devel
--- colcon_ws/src/git_clone.sh
+++ colcon_ws/src/git_clone.sh
@@ -1,2 +1,3 @@
+# https://index.ros.org/
 # git clone {URL}
 git clone -b $ROS_DISTRO https://github.com/micro-ROS/micro_ros_setup.git
